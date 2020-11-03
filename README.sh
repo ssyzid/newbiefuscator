aa7b00d5e85114620915cb53a03e773ce926bdb2
ssyzid <73906076+ssyzid@users.noreply.github.com>
2020-11-03 19:30:12 +0100

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,1 +1,1 @@
-<img src="https://cdn.discordapp.com/attachments/772597128987410503/773252135202324516/1029037.jpg"/>
+<h2 style="text-align: center;"><strong><img src="https://cdn.discordapp.com/attachments/772597128987410503/773252135202324516/1029037.jpg" alt="" width="720" height="360" />&nbsp; </strong></h2>
